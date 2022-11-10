b4402163a3eaa2558c780052969c3b72f22d9fb3
Majoros Máté <83972650+Dongesz@users.noreply.github.com>
2022-11-10 13:28:02 +0100

    Add files via upload

---
+++ python2feladatok.py
@@ -0,0 +1,41 @@
+'''print("Milyen napod volt ma?")
+valasz = input()
+if valasz == "igen":
+    print("mert kutya vagy")
+elif valasz == "nem":
+    print("jol is teszed")
+else :
+    print("nem értem csira") '''
+
+ 
+
+'''
+szam = int(input('kerek egy pozitive egesz szamot'))
+if szam % 2 == 0:
+    print('a szam paros')
+else:
+    print('a szam paratlan')'''
+
+'''
+import random
+
+gondolt_szam = random.randint(1,5)
+print('sugok', gondolt_szam)
+tipp =  int(input('gondoltam egy szamra tippel meg!'))
+if tipp == gondolt_szam:
+    print('eltalaltad')
+elif tipp < gondolt_szam:
+    print('a gondolt szam nagyobb ')
+elif tipp > gondolt_szam:
+    print('a gondolt szam kisebb')'''
+
+'''list = [1:10]
+for i in list (list % 2 == 0 ):
+    print(i)'''
+
+
+
+
+for i in range  (1,12):
+    i -= 1
+    print(i)
